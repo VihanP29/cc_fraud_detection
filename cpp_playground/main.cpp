124dc09215e22b0d34189ffb31a8f7f1a60e3cb3
Vihan <vihandajiba@gmail.com>
2025-12-30 13:41:20 +0530

    Initial Commit

---
+++ cpp_playground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_playground
+//
+//  Created by Vihan Patil on 12/30/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
